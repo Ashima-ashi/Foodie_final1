5bf6bc77a482b4d2bb74c2ebe472db75316fa6e8
Ashima-ashi <ashimagarg118@gmail.com>
2024-12-16 19:09:09 +0530

    update

--- Backend/config.js
+++ Backend/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     MONGODB_URI:  "mongodb+srv://ashimagarg118:9817820258@cluster0.6w2zv.mongodb.net/Foodie?retryWrites=true&w=majority&appName=Cluster0",
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT || 5000,
     HOST: process.env.HOST || "0.0.0.0"
 };
 
