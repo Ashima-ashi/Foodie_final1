eaa91b47a30101f9394215be2fab077c69fc9af7
Ashima-ashi <ashimagarg118@gmail.com>
2024-12-18 18:23:08 +0530

    link

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin:['http://localhost:3000'],
+  origin:['http://localhost:3000',process.env.ORIGIN_1,process.env.ORIGIN_2],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
